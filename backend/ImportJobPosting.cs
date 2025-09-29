6217b05c4bf9d0a5414fe902672f03265b4deb6d
Andrew Bradley <andrew.charles.bradley@gmail.com>
2025-09-29 11:14:26 -0400

    Remove API authorization for now

--- backend/ImportJobPosting.cs
+++ backend/ImportJobPosting.cs
@@ -17,7 +17,7 @@ public class ImportJobPosting
     }
 
     [Function("ImportJobPosting")]
-    public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequest req)
+    public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequest req)
     {
         try
         {

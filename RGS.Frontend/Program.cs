fb8c5e7539a6112bcbeff7cea350932cec3a4990
Andrew Bradley <andrew.charles.bradley@gmail.com>
2025-10-29 11:40:32 -0400

    Don't use environment variables because that doesn't play nicely with blazor wasm

--- RGS.Frontend/Program.cs
+++ RGS.Frontend/Program.cs
@@ -5,7 +5,6 @@ using RGS.Frontend;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-builder.Configuration.AddEnvironmentVariables("RGS_");
 
 string apiBaseAddress = builder.Configuration["Api:BaseAddress"] ?? throw new RGSException("ApiBaseAddress not configured");
 

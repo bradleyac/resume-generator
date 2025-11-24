0dc48582112c277d5681e6b993429cbd0d84f867
Andrew Bradley <andrew.charles.bradley@gmail.com>
2025-11-24 12:14:05 -0500

    Add logging

--- RGS.Backend/Middleware/EasyAuthMiddleware.cs
+++ RGS.Backend/Middleware/EasyAuthMiddleware.cs
@@ -4,16 +4,19 @@ using System.Text.Json;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Middleware;
+using Microsoft.Extensions.Logging;
 
 namespace RGS.Backend.Middleware;
 
 internal class EasyAuthMiddleware : IFunctionsWorkerMiddleware
 {
   private readonly FunctionExecutionDelegate _next;
+  private readonly ILogger<EasyAuthMiddleware> _logger;
 
-  public EasyAuthMiddleware(FunctionExecutionDelegate next)
+  public EasyAuthMiddleware(FunctionExecutionDelegate next, ILogger<EasyAuthMiddleware> logger)
   {
     _next = next;
+    _logger = logger;
   }
 
   public async Task Invoke(FunctionContext context, FunctionExecutionDelegate next)
@@ -22,6 +25,7 @@ internal class EasyAuthMiddleware : IFunctionsWorkerMiddleware
     var req = await context.GetHttpRequestDataAsync();
 
     var principalHeader = req.Headers.Single(kvp => kvp.Key == "X-MS-CLIENT-PRINCIPAL").ToString();
+    _logger.LogInformation("Easy Auth Principal Header: {PrincipalHeader}", principalHeader);
     if (!string.IsNullOrEmpty(principalHeader))
     {
       var decoded = Convert.FromBase64String(principalHeader);

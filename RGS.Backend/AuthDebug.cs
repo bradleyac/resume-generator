40b8120a0f73434df2b6411ff07f190eecfc3c77
Andrew Bradley <andrew.charles.bradley@gmail.com>
2025-11-24 12:06:55 -0500

    Add more debug info

--- RGS.Backend/AuthDebug.cs
+++ RGS.Backend/AuthDebug.cs
@@ -1,3 +1,4 @@
+using System.Security.Claims;
 using Azure.Core;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -16,16 +17,17 @@ public class AuthDebug
     }
 
     [Function("AuthDebug")]
-    public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequest req)
+    public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequest req, FunctionContext context)
     {
+        var user = context.Items["User"] as ClaimsPrincipal;
         var headers = new Dictionary<string, string>
         {
             { "X-MS-CLIENT-PRINCIPAL", req.Headers["X-MS-CLIENT-PRINCIPAL"].ToString() },
             { "X-MS-CLIENT-PRINCIPAL-ID", req.Headers["X-MS-CLIENT-PRINCIPAL-ID"].ToString() },
             { "X-MS-CLIENT-PRINCIPAL-IDP", req.Headers["X-MS-CLIENT-PRINCIPAL-IDP"].ToString() },
             { "X-MS-CLIENT-PRINCIPAL-NAME", req.Headers["X-MS-CLIENT-PRINCIPAL-NAME"].ToString() },
-            { "IsAuthenticated", req.HttpContext.User.Identity?.IsAuthenticated.ToString() ?? "false"},
-            { "UserName", req.HttpContext.User.Identity?.Name ?? "null" }
+            { "IsAuthenticated", user?.Identity?.IsAuthenticated.ToString() ?? "false"},
+            { "UserName", user?.Identity?.Name ?? "null" }
         };
 
         return new OkObjectResult(headers);

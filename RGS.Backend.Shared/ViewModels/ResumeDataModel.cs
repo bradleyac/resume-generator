1d770138b05e4f7327b734c5e5c1c4199c4033ff
Andrew Bradley <andrew.charles.bradley@gmail.com>
2025-11-26 16:16:02 -0500

    Add bonus files from last commit

---
+++ RGS.Backend.Shared/ViewModels/ResumeDataModel.cs
@@ -0,0 +1,160 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using RGS.Backend.Shared.Models;
+
+namespace RGS.Backend.Shared.ViewModels;
+
+public class JobModel
+{
+  [Required]
+  public string? Title { get; set; }
+  [Required]
+  public string? Company { get; set; }
+  [Required]
+  public string? Location { get; set; }
+  [Required]
+  public string? Start { get; set; }
+  [Required]
+  public string? End { get; set; }
+  [Required]
+  public List<string> Bullets { get; set; } = [];
+
+  public Job ValidatedUnwrap()
+  {
+    Validator.ValidateObject(this, new ValidationContext(this));
+    return new Job(Title!, Company!, Location!, Start!, End!, [.. Bullets]);
+  }
+}
+
+public class ProjectModel
+{
+  [Required]
+  public string? Name { get; set; }
+  [Required]
+  public string? Description { get; set; }
+  [Required]
+  public List<string> Technologies { get; set; } = [];
+  [Required]
+  public string? When { get; set; }
+  public Project ValidatedUnwrap()
+  {
+    Validator.ValidateObject(this, new ValidationContext(this));
+    return new Project(Name!, Description!, [.. Technologies], When!);
+  }
+}
+
+public class EducationModel
+{
+  [Required]
+  public string? Degree { get; set; }
+  [Required]
+  public string? School { get; set; }
+  [Required]
+  public string? Location { get; set; }
+  [Required]
+  public string? Graduation { get; set; }
+  public Education ValidatedUnwrap()
+  {
+    Validator.ValidateObject(this, new ValidationContext(this));
+    return new Education(Degree!, School!, Location!, Graduation!);
+  }
+}
+
+public class ContactModel
+{
+  [Required]
+  public string? Email { get; set; }
+  [Required]
+  public string? Phone { get; set; }
+  [Required]
+  public string? Github { get; set; }
+  public Contact ValidatedUnrawp()
+  {
+    Validator.ValidateObject(this, new ValidationContext(this));
+    return new Contact(Email!, Phone!, Github!);
+  }
+}
+
+public class SkillCategoryModel
+{
+  [Required]
+  public string? Label { get; set; }
+  [Required]
+  public List<string> Items { get; set; } = [];
+  public SkillCategory ValidatedUnwrap()
+  {
+    Validator.ValidateObject(this, new ValidationContext(this));
+    return new SkillCategory(Label!, [.. Items]);
+  }
+}
+
+public class BookModel
+{
+  [Required]
+  public string? Title { get; set; }
+  [Required]
+  public string? Author { get; set; }
+  public Book ValidatedUnwrap()
+  {
+    Validator.ValidateObject(this, new ValidationContext(this));
+    return new Book(Title!, Author!);
+  }
+}
+
+public class ResumeDataModel
+{
+  [Required]
+  public string? id { get; set; }
+  [Required]
+  public bool IsMaster { get; set; }
+  [Required]
+  public string? Name { get; set; }
+  [Required]
+  public string? Title { get; set; }
+  [Required]
+  public string? About { get; set; }
+  [Required]
+  public string? StreetAddress { get; set; }
+  [Required]
+  public string? City { get; set; }
+  [Required]
+  public string? State { get; set; }
+  [Required]
+  public string? Zip { get; set; }
+  [Required]
+  public ContactModel? Contact { get; set; }
+  [Required]
+  public List<JobModel> Jobs { get; set; } = [];
+  [Required]
+  public List<ProjectModel> Projects { get; set; } = [];
+  [Required]
+  public List<EducationModel> Education { get; set; } = [];
+  [Required]
+  public List<SkillCategoryModel> Skills { get; set; } = [];
+  [Required]
+  public List<BookModel> Bookshelf { get; set; } = [];
+  public ResumeData ValidatedUnwrap(string userId)
+  {
+    Validator.ValidateObject(this, new ValidationContext(this));
+    return new ResumeData
+    (
+      id!,
+      userId,
+      IsMaster,
+      Name!,
+      Title!,
+      About!,
+      StreetAddress!,
+      City!,
+      State!,
+      Zip!,
+      Contact!.ValidatedUnrawp(),
+      [.. Jobs.Select(j => j.ValidatedUnwrap())],
+      [.. Projects.Select(p => p.ValidatedUnwrap())],
+      [.. Education.Select(e => e.ValidatedUnwrap())],
+      [.. Skills.Select(s => s.ValidatedUnwrap())],
+      [.. Bookshelf.Select(b => b.ValidatedUnwrap())]
+    );
+  }
+}
---
+++ RGS.Frontend/Pages/MasterResumeData.razor.cs
@@ -0,0 +1,60 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using R3;
+using RGS.Backend.Shared.ViewModels;
+
+namespace RGS.Frontend.Pages;
+
+public partial class MasterResumeData : ComponentBase, IDisposable
+{
+  private EditForm? editForm;
+  private ResumeDataModel resumeData = null!;
+  private bool _disposedValue;
+  private IDisposable? _subscription;
+
+  [Inject] private IResumeDataService ResumeDataService { get; set; } = null!;
+
+  protected override async Task OnInitializedAsync()
+  {
+    // Load master resume data
+    resumeData = await ResumeDataService.GetMasterResumeDataAsync();
+    _subscription?.Dispose();
+    _subscription = Observable.FromEventHandler<FieldChangedEventArgs>(
+      a => editForm!.EditContext!.OnFieldChanged += a,
+      a => editForm!.EditContext!.OnFieldChanged -= a)
+      .Debounce(TimeSpan.FromMilliseconds(500))
+      .Where(_ => editForm!.EditContext!.Validate())
+      .Do(async () =>
+      {
+        editForm!.EditContext!.MarkAsUnmodified();
+        await HandleValidSubmit(editForm.EditContext);
+      })
+      .Subscribe();
+  }
+
+  private async Task HandleValidSubmit(EditContext args)
+  {
+    ResumeDataModel data = (ResumeDataModel)args.Model;
+    await ResumeDataService.SetMasterResumeDataAsync(data);
+  }
+
+  protected virtual void Dispose(bool disposing)
+  {
+    if (!_disposedValue)
+    {
+      if (disposing)
+      {
+        _subscription?.Dispose();
+      }
+
+      _disposedValue = true;
+    }
+  }
+
+  public void Dispose()
+  {
+    // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+    Dispose(disposing: true);
+    GC.SuppressFinalize(this);
+  }
+}

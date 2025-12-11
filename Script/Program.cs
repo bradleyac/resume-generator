5ee2b9d3df7c51d4530f3c938410dd8d030afdea
Andrew Bradley <andrew.charles.bradley@gmail.com>
2025-12-11 11:43:19 -0500

    Add and run script to convert prod to new schema

---
+++ Script/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Drawing.Text;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Cosmos.Linq;
+using RGS.Backend.Shared.Models;
+
+var connectionString = "my_connection_string";
+
+var cosmosClient = new CosmosClient(connectionString);
+
+var container = cosmosClient.GetContainer("Resumes", "ResumeData");
+
+var iterator = container.GetItemLinqQueryable<ResumeDataOld>().ToFeedIterator();
+while (iterator.HasMoreResults)
+{
+  var response = await iterator.ReadNextAsync();
+  bool once = false;
+  foreach (var item in response.Where(i => i?.Name != null))
+  {
+    if (once) return;
+    Console.WriteLine(item.id);
+    var newItem = new ResumeData(item.id, item.UserId, item.IsMaster, new Bio(item.Name, item.Title, item.About, item.StreetAddress ?? "74 E Charlton Rd", item.City, item.State, item.Zip ?? "01562"), item.Contact, item.Jobs, item.Projects, item.Education, item.Skills, item.Bookshelf, item.GeneratedRankings, item.CoverLetter);
+    container.UpsertItemAsync(newItem, new PartitionKey(newItem.id)).GetAwaiter().GetResult();
+  }
+}
+
+record ResumeDataOld(string id,
+                         string UserId,
+                         bool IsMaster,
+                         string Name, string Title, string About, string StreetAddress, string City, string State, string Zip,
+                         Contact Contact,
+                         Job[] Jobs,
+                         Project[] Projects,
+                         Education[] Education,
+                         SkillCategory[] Skills,
+                         Book[] Bookshelf,
+                         Rankings? GeneratedRankings = null,
+                         string? CoverLetter = null);

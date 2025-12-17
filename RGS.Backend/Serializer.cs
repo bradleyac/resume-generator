f95747f6a6a27567b82d7c3a37570aba38b8289f
Andrew Bradley <andrew.charles.bradley@gmail.com>
2025-12-17 13:44:40 -0500

    Remove extra code and fix build

--- RGS.Backend/Serializer.cs
+++ RGS.Backend/Serializer.cs
@@ -15,7 +15,6 @@ public class CosmosSystemTextJsonSerializer : CosmosSerializer
     _jsonSerializerOptions = new JsonSerializerOptions
     {
       WriteIndented = false,
-      AllowOutOfOrderMetadataProperties = true,
     };
   }
 

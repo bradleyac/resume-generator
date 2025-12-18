ba2bb4ba0ae7b8f42e4780906a688baa9e36cce4
Andrew Bradley <andrew.charles.bradley@gmail.com>
2025-12-18 11:44:15 -0500

    Update script

--- Script/Program.cs
+++ Script/Program.cs
@@ -15,7 +15,7 @@ var users = cosmosClient.GetContainer("Resumes", "Users");
 
 const string masterResumeId = "13b1c25378654837956349833d60216e";
 var masterResumeData = await resumeData.ReadItemAsync<ResumeDataOld>(masterResumeId, new PartitionKey(masterResumeId));
-var newSourceResumeData = new SourceResumeData(Guid.NewGuid().ToString(), masterResumeData.Resource.UserId, new Bio(masterResumeData.Resource.Name, masterResumeData.Resource.Title, masterResumeData.Resource.About, masterResumeData.Resource.StreetAddress, masterResumeData.Resource.City, masterResumeData.Resource.State, masterResumeData.Resource.Zip), masterResumeData.Resource.Contact, masterResumeData.Resource.Jobs, masterResumeData.Resource.Projects, masterResumeData.Resource.Education, masterResumeData.Resource.Skills, masterResumeData.Resource.Bookshelf);
+var newSourceResumeData = new SourceResumeData(Guid.NewGuid().ToString(), masterResumeData.Resource.UserId, masterResumeData.Resource.Bio, masterResumeData.Resource.Contact, masterResumeData.Resource.Jobs, masterResumeData.Resource.Projects, masterResumeData.Resource.Education, masterResumeData.Resource.Skills, masterResumeData.Resource.Bookshelf);
 
 await userData.UpsertItemAsync<UserDataRecord>(newSourceResumeData, new PartitionKey(newSourceResumeData.UserId));
 
@@ -32,7 +32,7 @@ while (iterator.HasMoreResults)
     var oldResumeData = (await resumeData.ReadItemAsync<ResumeDataOld>(item.id, new PartitionKey(item.id))).Resource;
     var oldCoverLetter = oldResumeData.CoverLetter;
     var oldUser = (await users.ReadItemAsync<OldUser>(item.UserId, new PartitionKey(item.UserId))).Resource;
-    var newPosting = new JobPosting(item.id, item.UserId, item.ImportedAt, new PostingDetails(item.Link, item.Company, item.Title, item.PostingText, item.StreetAddress, item.City, item.State, item.Zip), oldResumeData == null ? null : new ResumeData(item.id, oldResumeData.UserId, new Bio(oldResumeData.Name, oldResumeData.Title, oldResumeData.About, oldResumeData.StreetAddress, oldResumeData.City, oldResumeData.State, oldResumeData.Zip), oldResumeData.Contact, oldResumeData.Jobs, oldResumeData.Projects, oldResumeData.Education, oldResumeData.Skills, oldResumeData.Bookshelf, oldResumeData.GeneratedRankings), oldCoverLetter is null ? null : new CoverLetter(item.id, item.UserId, oldCoverLetter), item.Status.ToString());
+    var newPosting = new JobPosting(item.id, item.UserId, item.ImportedAt, new PostingDetails(item.Link, item.Company, item.Title, item.PostingText, item.StreetAddress, item.City, item.State, item.Zip), oldResumeData == null ? null : new ResumeData(item.id, oldResumeData.UserId, oldResumeData.Bio, oldResumeData.Contact, oldResumeData.Jobs, oldResumeData.Projects, oldResumeData.Education, oldResumeData.Skills, oldResumeData.Bookshelf, oldResumeData.GeneratedRankings), oldCoverLetter is null ? null : new CoverLetter(item.id, item.UserId, oldCoverLetter), item.Status.ToString());
     await userData.UpsertItemAsync<UserDataRecord>(new RGS.Backend.Shared.Models.User(oldUser.id, oldUser.id, oldUser.UserDetails, oldUser.ApiKey, newSourceResumeData.id), new PartitionKey(oldUser.id));
     await userData.UpsertItemAsync<UserDataRecord>(newPosting, new PartitionKey(newPosting.UserId));
     // Console.WriteLine(JsonSerializer.Serialize<UserDataRecord>(newPosting));
@@ -40,5 +40,5 @@ while (iterator.HasMoreResults)
 }
 
 public record JobPostingOld(string id, string UserId, string Link, string Company, string Title, string PostingText, DateTime ImportedAt, string? StreetAddress = null, string? City = null, string? State = null, string? Zip = null, string Status = PostingStatus.Pending);
-public record ResumeDataOld(string id, string UserId, bool IsMaster, string Name, string Title, string About, string StreetAddress, string City, string State, string Zip, Contact Contact, Job[] Jobs, Project[] Projects, Education[] Education, SkillCategory[] Skills, Book[] Bookshelf, Rankings? GeneratedRankings = null, string? CoverLetter = null);
+public record ResumeDataOld(string id, string UserId, Bio Bio, Contact Contact, Job[] Jobs, Project[] Projects, Education[] Education, SkillCategory[] Skills, Book[] Bookshelf, Rankings? GeneratedRankings = null, string? CoverLetter = null);
 public record OldUser(string id, string UserDetails, string ApiKey);
